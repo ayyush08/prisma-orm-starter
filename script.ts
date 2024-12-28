21e431efc35ce56bc9706388893de8f45a34a1b3
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-28 19:41:46 +0530

    Client Create Ops

--- script.ts
+++ script.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+    // log:["query"] //logs all queries
+}) // manages different connnections for us, if db contains 5 different connections prisma will handle that for us, So it's really important to keep it in check that we only use one instance of this client otherwise it'll bog down our database
 
 async function main(){
     //we write our prisma client queries here
@@ -14,9 +16,61 @@ async function main(){
     // console.log(user)
 
     //Fina all users
+    // console.log(await prisma.user.findMany());
     
     //delete all users
-    await prisma.user.deleteMany()
+    // await prisma.user.deleteMany()
+
+    //------------------
+    //Create - (use createMany and pass an Array of users to create multiple users at once)
+    // const user = await prisma.user.create({
+    //     data:{
+    //         name:"Ayush",
+    //         email:"curator@gmail.com",
+    //         age:24,
+    //         userPreference:{
+    //             create:{
+    //                 emailUpdates: true
+    //             }
+    //         }
+    //     },
+        //YOU CAN ONLY DO WITHER SELECT OR INCLUDE AT ONE TIME AS PER USE CASE
+        //INCLUDE will include relations
+        //SELECT will let you hard code exact values you want from current or nest it too with related ones
+        // include:{
+        //     userPreference: true
+        // },
+        // select:{
+        //     name:true,
+        //     userPreference:{
+        //         select:{
+        //             id:true
+        //         }
+        //     }
+        // }
+    // })
+
+    // const users = await prisma.user.createMany({
+    //     data:[
+    //         {
+    //             name:"Ayush",
+    //             email:"ayush@gmail.com",
+    //             age:20
+    //         },
+    //         {
+    //             name:"Curator",
+    //             email:"curator@gmail.com",
+    //             age:21
+    //         }
+    //     ]
+    //     //NOTE: cannot use select clause with createMany
+    // })
+    // console.log(users);
+    
+
+
+
+
 }
 
 main()
